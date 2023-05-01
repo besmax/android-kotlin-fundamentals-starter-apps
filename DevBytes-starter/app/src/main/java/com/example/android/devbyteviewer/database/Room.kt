b18b173eab0dfea662a4a87a1817cc8e083c1c9f
Max <bespalov.m.9@ya.ru>
2023-05-01 11:55:28 +0300

    complete task, but problems were appeared: sdk ver. was checnged, in devbyte_item layout error without adding 'Theme.MaterialComponents'

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -13,5 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll( videos: List<DatabaseVideo>)
+}
+
+
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
+}
